b7ff5ae5099a31a9ba3c68ac32c95139691b2278
fabio.sousa <fabiossilva00@gmail.com>
2019-05-23 17:29:46 -0300

    Pokemon Brinks

--- app/src/services/request/getPeople.js
+++ app/src/services/request/getPeople.js
@@ -1,17 +1,31 @@
 import { urlBase } from '../urlBaseSWAPI'
 import { fetchData, fetchDataFulfilled, fetchDataRejected } from '../../redux/reducers/peopleReducer'
-
+import axios from 'axios'
+ 
 export const getPeople = () => {
     return async dispatch => {
         dispatch(fetchData(true))
         urlBase.get('/people')
             .then(res => {
                 console.log(res.data.results)
+                console.log(dispatch)
                 dispatch(fetchDataFulfilled(res.data.results))
+                setTimeout(() => {dispatch(getPokemon())}, 3000)
+                // dispatch(getPokemon())
+                
             })
             .catch(err => {
                 console.log(err.message)
                 dispatch(fetchDataRejected(String(err.message)))
             })
+    }
+}
+
+
+export const getPokemon = () => {
+    return async dispatch => {
+        axios.get('https://pokeapi.co/api/v2/ability/')
+            .then(res => dispatch(fetchDataFulfilled(res.data.results)))
+            .catch(err => console.log(err))
     }
 }
